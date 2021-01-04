ebe1752af23a7848362f66c96f8248faa50a8b8f
rrofi <rrofi@yahoo.com>
2021-01-04 10:03:50 +0700

    yrdy

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("tste");
+            Console.WriteLine("tste2");
             Console.ReadLine();
         }
     }
